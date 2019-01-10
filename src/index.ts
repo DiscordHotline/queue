3369ade44f87fb38aad06d2208c5c937fe30cf88
Aaron Scherer <aequasi@gmail.com>
2019-01-09 16:28:22 -0800

    Using flatted

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {Channel, connect, Message as AMQPMessage} from 'amqplib';
 import {AxiosInstance, default as axios} from 'axios';
 import * as hookcord from 'hookcord';
-import {parse, stringify} from 'flatted'
+import {stringify, parse} from 'flatted';
 import * as moment from 'moment';
 
 import * as interfaces from './interfaces';
